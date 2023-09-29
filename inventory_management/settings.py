94d2166aed2319c07a5f9a937865cc46e8f4cd00
salvator-del <brianmwisya826@gmail.com>
2023-09-29 17:28:59 +0300

    Added staticfiles for admin

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- inventory_management/urls.py
+++ inventory_management/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("inventory.urls"))
 ]
+urlpatterns += staticfiles_urlpatterns() # new
